37d70d4395e81cf8659a1fc2f73e3697c13873ba
kkoouuya0730 <kouya.0730@icloud.com>
2024-09-12 22:35:09 +0900

    test

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,8 +1,5 @@
-import RequiredLabel from "@/components/elements/text/RequiredLabel";
 import Layout from "@/components/layouts/Layout";
 import LoginForm from "@/features/login/LoginForm";
-import { MyInput } from "@/features/menu/MyInput";
-import { useRef, useState } from "react";
 
 export default function Login() {
   return (
