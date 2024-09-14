36d97d991e4c5a217d85ab06434d234182c25eb4
kkoouuya0730 <kouya.0730@icloud.com>
2024-09-14 14:50:52 +0900

    Add Icons

--- src/ui/icon/ClockIcon.tsx
+++ src/ui/icon/ClockIcon.tsx
@@ -1,22 +1,19 @@
-export function ClockIcon() {
+import { SVGProps } from "react";
+
+export function ClockIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
       height="1em"
-      viewBox="0 0 21 21"
+      viewBox="0 0 24 24"
+      {...props}
     >
-      <g
-        fill="none"
+      <path
+        fill="currentColor"
         fillRule="evenodd"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        transform="matrix(-1 0 0 1 19 2)"
-      >
-        <circle cx="8.5" cy="8.5" r="8"></circle>
-        <path d="M8.5 5.5v4H5"></path>
-      </g>
+        d="m12.6 11.503l3.891 3.891l-.848.849L11.4 12V6h1.2zM12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10m0-1.2a8.8 8.8 0 1 0 0-17.6a8.8 8.8 0 0 0 0 17.6"
+      ></path>
     </svg>
   );
 }
