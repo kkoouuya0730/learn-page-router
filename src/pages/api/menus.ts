4d9194ebb4212a5d1e507f2a13cd9a35fa12ce90
kkoouuya0730 <kouya.0730@icloud.com>
2024-09-22 22:29:40 +0900

    Create Menu API

---
+++ src/pages/api/menus.ts
@@ -0,0 +1,49 @@
+import { Menu } from "@/types/menu";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type ResponseData = {
+  menus: Menu[];
+};
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  const menus = [
+    {
+      name: "Classic Coffee",
+      price: 450,
+      imgSrc: "https://picsum.photos",
+      description:
+        "A smooth and rich blend of coffee made with the finest beans, perfect for any time of the day.",
+    },
+    {
+      name: "Vanilla Latte",
+      price: 520,
+      imgSrc: "https://picsum.photos",
+      description:
+        "A creamy latte with a hint of vanilla, creating the perfect balance of sweetness and coffee flavor.",
+    },
+    {
+      name: "Mocha Frappuccino",
+      price: 580,
+      imgSrc: "https://picsum.photos",
+      description:
+        "A cold and refreshing mocha blended with ice and topped with whipped cream and chocolate drizzle.",
+    },
+    {
+      name: "Hazelnut Cappuccino",
+      price: 500,
+      imgSrc: "https://picsum.photos",
+      description:
+        "A rich and foamy cappuccino infused with the nutty flavor of hazelnuts for a delightful treat.",
+    },
+    {
+      name: "Iced Americano",
+      price: 400,
+      imgSrc: "https://picsum.photos",
+      description:
+        "A bold and refreshing iced coffee, perfect for a hot day and those who prefer their coffee without milk.",
+    },
+  ];
+  res.status(200).json({ menus });
+}
