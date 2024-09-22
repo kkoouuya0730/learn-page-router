9f8f0bf1ed7ff32d75104694915ce01d79ba9fde
kkoouuya0730 <kouya.0730@icloud.com>
2024-09-22 14:33:47 +0900

    Refactoring Login Form and Create TextForm

--- src/components/elements/form/SearchForm/index.tsx
+++ src/components/elements/form/SearchForm/index.tsx
@@ -1,16 +1,9 @@
-import { SearchIcon } from "@/ui/icon/SearchIcon";
+import { TextForm } from "../TextForm";
 
 export default function SearchForm({}) {
   return (
-    <>
-      <div className="relative">
-        <SearchIcon className="absolute left-2 top-1/2 h-5 w-5 -translate-y-1/2 text-amber-500" />
-        <input
-          type="text"
-          className="border rounded-lg pl-10 text-amber-700"
-          aria-label="検索"
-        />
-      </div>
-    </>
+    <div>
+      <TextForm isRequired={false} iconType={"search"} />
+    </div>
   );
 }
