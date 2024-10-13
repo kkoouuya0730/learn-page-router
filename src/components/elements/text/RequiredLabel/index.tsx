425e95b8f515d622bf4ac8f5d8ed043b705be225
kkoouuya0730 <kouya.0730@icloud.com>
2024-10-13 09:07:10 +0900

    Refactoring Auth Page

--- src/components/elements/text/RequiredLabel/index.tsx
+++ src/components/elements/text/RequiredLabel/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function RequiredLabel() {
   return (
     <span className="bg-red-500 rounded-lg text-white inline-block px-2">
-      必須
+      Required
     </span>
   );
 }
