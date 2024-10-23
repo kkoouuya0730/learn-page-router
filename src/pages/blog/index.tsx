534e8e7f9dfae242024db0ad8df2a7da1e874e21
kkoouuya0730 <kouya.0730@icloud.com>
2024-10-23 09:38:13 +0900

    Refactoring Blog Files

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -2,11 +2,11 @@ import CustomHeading from "@/components/elements/text/CustomHeading";
 import Layout from "@/components/layouts/Layout";
 import { Blog } from "@/types/blog";
 import { BlogListItem } from "./parts/BlogListItem";
+import { createClient } from "@/lib/supabase/server-props";
+import { GetServerSidePropsContext } from "next";
 
 type Props = {
-  data: {
-    blogs: Blog[];
-  };
+  data: Blog[];
 };
 export default function BlogPage({ data }: Props) {
   return (
@@ -15,7 +15,7 @@ export default function BlogPage({ data }: Props) {
         <CustomHeading tag="h1">最新の記事</CustomHeading>
       </div>
       <div className="grid lg:grid lg:grid-cols-2 gap-4">
-        {data.blogs.map((blog) => (
+        {data.map((blog) => (
           <BlogListItem blog={blog} key={blog.title} />
         ))}
       </div>
@@ -27,11 +27,21 @@ BlogPage.getLayout = function getLayout(page: any) {
   return <Layout>{page}</Layout>;
 };
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const supabase = createClient(context);
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
   try {
-    const res = await fetch(
-      "http://localhost:3000/api/blogs"
-    );
+    const res = await fetch("http://localhost:3000/api/blogs");
     if (!res.ok) {
       throw new Error(`HTTP error! status: ${res.status}`);
     }
